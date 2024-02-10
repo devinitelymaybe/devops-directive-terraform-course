ba05dd55a3e794819b2307e2006c0f451016e4a2
devinitelymaybe <159635234+devinitelymaybe@users.noreply.github.com>
2024-02-10 18:15:53 +0100

    update region and ami

--- 02-overview/main.tf
+++ 02-overview/main.tf
@@ -8,10 +8,10 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = "eu-west-1"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-011899242bb902164" # Ubuntu 20.04 LTS // us-east-1
+  ami           = "ami-0905a3c97561e0b69" # Ubuntu 20.04 LTS // eu-west-1
   instance_type = "t2.micro"
 }
