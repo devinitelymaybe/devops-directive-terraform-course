c2d37d2b7989a7403eef1f55dd6ac22d6c5720a6
devinitelymaybe <159635234+devinitelymaybe@users.noreply.github.com>
2024-02-11 10:26:27 +0100

    dfdf

--- 02-overview/backend.tf
+++ 02-overview/backend.tf
@@ -1,11 +1,22 @@
-# Using a single workspace:
-terraform {
-  backend "remote" {
-    hostname = "app.terraform.io"
-    organization = "devmaybe"
-
-    workspaces {
-      name = "devops-directive-terraform-course"
+# create s3
+resource "aws_s3_bucket" "rmtfstate34654353" {
+    bucket = "rmtfstate34654353"
+    versioning {
+        enabled = true
     }
-  }
+    server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+    }
+}
+
+# create dynamodb
+resource "aws_dynamo_db_table" "statelock" {
+    name = "statelock"
+    billing_mode = "PAY_PER_REQUEST"
+    hash_key = "LockID"
+
+    attribute {
+        name = "LockID"
+        type = "S"
+    }
 }
